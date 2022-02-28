bf0fc9840b14928363a68b59a4e92300c063d28d
Damhee Kim <ekal.kdh@gmail.com>
2022-02-28 23:52:59 +0900

    make dialog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled, { ThemeProvider } from "styled-components";
 import Button from "./components/Button";
+import Dialog from "./components/Dialog";
 
 const AppBlock = styled.div`
   width: 512px;
@@ -55,6 +56,13 @@ function App() {
             BUTTON
           </Button>
         </ButtonGroup>
+        <Dialog
+          title="정말로 삭제하시겠습니까?"
+          confirmText="삭제"
+          cancelText="취소"
+        >
+          데이터를 정말로 삭제하시겠습니까?
+        </Dialog>
       </AppBlock>
     </ThemeProvider>
   );
